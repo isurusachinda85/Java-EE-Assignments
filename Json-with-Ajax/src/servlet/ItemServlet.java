11094985f8f62ee59a1b0a8600540239b301bc72
isurusachinda85 <isurusachintha8@gmail.com>
2023-09-03 20:56:38 +0530

    update

--- Json-with-Ajax/src/servlet/ItemServlet.java
+++ Json-with-Ajax/src/servlet/ItemServlet.java
@@ -1,4 +1,16 @@
 package servlet;
 
-public class ItemServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/item")
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(2);
+    }
 }
